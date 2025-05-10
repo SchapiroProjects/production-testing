4cc97dfaae2814e81695f2a000d830727623f0cb
rschapiro <rasmus.schapiro@gmail.com>
2025-05-10 23:24:56 +0200

    last attempt

--- app.config.ts
+++ app.config.ts
@@ -4,12 +4,19 @@ import { defineConfig } from '@tanstack/react-start/config'
 export default defineConfig({
   tsr: {
     appDirectory: 'src',
+    apiBase: '/production/api',
   },
   server: {
     preset: 'bun',
   },
   routers: {
+    server: {
+      base: '/production/_server',
+    },
     client: {
+      base: '/production/_build',
+    },
+    public: {
       base: '/production',
     },
   },
